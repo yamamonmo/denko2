1c329f2ee2e08a76c5a8585f3f2283c3f809b8b9
Yamamoto <Nekozame0@gmail.com>
2026-07-16 15:26:04 +0900

    gitpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'denko2'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
